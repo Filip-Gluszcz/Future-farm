434f259b630392aa5a5575e6826e40ec3795bbc4
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-03-12 00:20:11 +0100

    whitenoise static files fix

--- FutureFarm/settings/base.py
+++ FutureFarm/settings/base.py
@@ -114,7 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 
