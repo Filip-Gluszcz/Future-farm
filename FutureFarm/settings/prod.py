286fd80e7f6f81a43721378de0a580f4d16a53cc
Filip-Gluszcz <80832642+Filip-Gluszcz@users.noreply.github.com>
2022-12-12 21:35:54 +0100

    Railway deploy_2 (bad request 400 > ALLOWED_HOSTS)

--- FutureFarm/settings/prod.py
+++ FutureFarm/settings/prod.py
@@ -7,10 +7,12 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'future-farm.herokuapp.com',
-    'https://web-production-dafa.up.railway.app',
-    ]
+ALLOWED_HOSTS = ['*']
+
+#ALLOWED_HOSTS = [
+#    'future-farm.herokuapp.com',
+#    'https://web-production-dafa.up.railway.app',
+#    ]
 
 #CELERY_BROKER_URL = os.environ['REDIS_URL']
 
